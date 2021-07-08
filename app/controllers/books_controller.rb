d80d932ee2c6a62c901eb7c24b27939bf06721a4
YamatoUeno-0403 <giveon0403@gmail.com>
2021-07-08 09:11:26 +0000

    compleate show

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,6 +16,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
+    flash[:success] = "Book was successfully created."
     redirect_to book_path(book.id)
   end
 
@@ -27,12 +28,14 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
+    flash[:success] = "Book was successfully updated."
     redirect_to book_path
   end
   
   def destroy
     @book = Book.find(params[:id])
     book.destroy
+    flash[:success] ="Book was successfully destroyed."
     redirect_to books_path
   end
   
